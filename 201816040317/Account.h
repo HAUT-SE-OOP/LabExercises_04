44104de62f8b9f3f73f32939605bae879226400f
201816040317 <56635386+201816040317@users.noreply.github.com>
2019-12-15 16:12:18 +0800

    Update Account.h

--- 201816040317/Account.h
+++ 201816040317/Account.h
@@ -1,57 +1,21 @@
-// Lab 1: Account.cpp
-// Member-function definitions for class Account.
-#include <iostream>
-using namespace std;
+// Lab 1: Account.h
+// Definition of Account class.
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
 
-#include "Account.h" // include definition of class Account
-
-// Account constructor initializes data member balance
-Account::Account( double initialBalance )
+class Account
 {
-   // if initialBalance is greater than or equal to 0.0, set this value 
-   // as the balance of the Account
-   if ( initialBalance >= 0.0 )
-      balance = initialBalance;
-   else // otherwise, output message and set balance to 0.0
-   {
-      cout << "Error: Initial balance cannot be negative." << endl;
-      balance = 0.0;
-   } // end if...else
-} // end Account constructor
+public:
+   Account( double ); // constructor initializes balance
+   virtual void credit(double);/* Write a function prototype for virtual function credit */
+   virtual bool debit(double);/* Write a function prototype for virtual function debit */
+   void setBalance( double ); // sets the account balance
+   double getBalance(); // return the account balance
+private:
+   double balance; // data member that stores the balance
+}; // end class Account
 
-// credit (add) an amount to the account balance
-void Account::credit( double amount )
-{
-   balance = balance + amount; // add amount to balance
-} // end function credit
-
-// debit (subtract) an amount from the account balance
-// return bool indicating whether money was debited
-bool Account::debit( double amount )
-{
-   if ( amount > balance ) // debit amount exceeds balance
-   {
-      cout << "Debit amount exceeded account balance." << endl;
-      return false;
-   } // end if
-   else // debit amount does not exceed balance
-   {
-      balance = balance - amount;
-      return true;
-   } // end else
-} // end function debit
-
-// set the account balance
-void Account::setBalance( double newBalance )
-{
-   balance = newBalance;
-} // end function setBalance
-
-// return the account balance
-double Account::getBalance()
-{
-   return balance;
-} // end function getBalance
+#endif
 
 
 /**************************************************************************
